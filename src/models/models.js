c7c407dbc8248e107a5459e177080f332312c7d1
Carlos Mur <carlos.mur-estudillos@capgemini.com>
2022-02-01 16:30:07 +0100

    fix: Chema error solved

--- src/models/models.js
+++ src/models/models.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
-export const ProductsSchema = new Schema({
-  name: {type: String, require: 'Introduce un producto.'},
-  description: {type: String, require: 'Introduce una descripcion'},
+
+export const ProductSchema = new Schema({
+  name: {type: String, required: 'Introduce un producto.'},
+  description: {type: String, required: 'Introduce una descripcion'},
   category: {type: String},
   price: {type: Number},
   created_date: { type: Date, default: Date.now},
